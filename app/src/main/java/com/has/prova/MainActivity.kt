e7a33503ef9a311f06e37606de9b59e992a2908d
peddrovieira <pe.bonangelo@gmail.com>
2023-04-12 21:58:55 -0300

    Correções

--- app/src/main/java/com/has/prova/MainActivity.kt
+++ app/src/main/java/com/has/prova/MainActivity.kt
@@ -27,43 +27,41 @@ class MainActivity : AppCompatActivity() {
         var IMC = peso/(altura*altura)
 
         if(IMC <18.5){
-            val pesoImage: ImageView = findViewById(R.id.imageView4)
+            val pesoImage: ImageView = findViewById(R.id.img)
             pesoImage.setImageResource(R.drawable.abaixodopeso)
-
+            binding.imcvalor.text = IMC.toString()
         }
         else if(IMC <18.5){
-            val pesoImage: ImageView = findViewById(R.id.imageView4)
+            val pesoImage: ImageView = findViewById(R.id.img)
             pesoImage.setImageResource(R.drawable.pesoideal)
             binding.imcvalor.text = IMC.toString()
 
         }
         else if(IMC > 18.6 && IMC < 24.9){
-            val pesoImage: ImageView = findViewById(R.id.imageView4)
+            val pesoImage: ImageView = findViewById(R.id.img)
             pesoImage.setImageResource(R.drawable.sobre_peso)
             binding.imcvalor.text = IMC.toString()
 
         }
         else if(IMC <25 && IMC <29.9){
-            val pesoImage: ImageView = findViewById(R.id.imageView4)
+            val pesoImage: ImageView = findViewById(R.id.img)
             pesoImage.setImageResource(R.drawable.iniciodaobesidade)
             binding.imcvalor.text = IMC.toString()
 
         }
         else if(IMC > 30 && IMC > 34.9){
-            val pesoImage: ImageView = findViewById(R.id.imageView4)
+            val pesoImage: ImageView = findViewById(R.id.img)
             pesoImage.setImageResource(R.drawable.obesidade)
             binding.imcvalor.text = IMC.toString()
 
         }
         else{
-            val pesoImage: ImageView = findViewById(R.id.imageView4)
+            val pesoImage: ImageView = findViewById(R.id.img)
             pesoImage.setImageResource(R.drawable.excessodeobesidade)
             binding.imcvalor.text = IMC.toString()
 
         }
 
-
         }
 
     }
-}
